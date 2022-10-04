45b6d49a6ca0604c6b4720ff8d821ce5267ce176
Ramya2001bca <113408822+Ramya2001bca@users.noreply.github.com>
2022-10-04 11:15:29 +0530

    Add files via upload

---
+++ Worker.java
@@ -0,0 +1,20 @@
+public class Worker{
+final String Workername="Ramya";
+int Workerage=21;
+long phonenumber;
+float weight;
+void salary(int salary){
+System.out.println(salary);
+}
+void family(int familymembers){
+System.out.println(familymembers);
+}
+public int add(int a,int b){
+int c=a+b;
+System.out.println(c);
+return c;
+}
+void musiclover(){
+System.out.println("musiclover");
+}
+}
---
+++ Worker2.java
@@ -0,0 +1,17 @@
+public class Worker2{
+public static void main(String[] args){
+Worker ramya=new Worker();
+//ramya.Workername();
+//ramya.Workerage();
+ramya.phonenumber=1235667790l;
+ramya.weight=42.5f;
+System.out.println(ramya.Workername);
+System.out.println(ramya.Workerage);
+System.out.println(ramya.phonenumber);
+System.out.println(ramya.weight);
+ramya.salary(5000);
+ramya.family(4);
+ramya.add(5,5);
+ramya.musiclover("vijay");
+}
+}
